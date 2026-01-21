329e915eccc6303d57586c427623440a30ab2c37
murphy-afk <bardarenoemi@gmail.com>
2026-01-21 15:56:40 +0100

    feat: avg vote

--- src/pages/MovieIndex.jsx
+++ src/pages/MovieIndex.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 export default function MovieIndex() {
-
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -15,25 +14,35 @@ export default function MovieIndex() {
       .catch((err) => {
         console.log(err);
       });
-  },
-    []);
+  }, []);
 
   const backendBaseUrl = import.meta.env.VITE_BACKEND_URL;
+
   return (
     <>
       <section className="py-5 mt-5 container">
-        <h1>Movie list</h1>
-        <div className="row row-cols-3 gap-2">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <h1 className="fw-bold">Movie List</h1>
+        </div>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
           {movies.map((movie) => (
-            <div className="col mb-4" key={movie.id}>
-              <div className="card h-100 shadow-sm"  >
-                {/* TODO: FIND IMAGE PLACEHOLDER, IMPORT AND SUBSTITUTE TO 'no_image_placeholder' */}
-                <img src={movie.image !== null ? `${backendBaseUrl}/movies_cover/${movie.image}` : "no_image_placeholder.jpg"} className="card-img-top" alt={movie.title} />
+            <div className="col" key={movie.id}>
+              <div className="card h-100 shadow-sm border-0 movie-card">
+                <img src={movie.image !== null
+                      ? `${backendBaseUrl}/movies_cover/${movie.image}`
+                      : "no_image_placeholder.jpg"
+                  }
+                  className="card-img-top"
+                  alt={movie.title}
+                />
                 <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">{movie.title}</h5>
+                     <span className="badge text-bg-warning w-25 my-1">
+                      Vote: {movie.avg_vote}
+                    </span>
+                  <h5 className="card-title fw-semibold">{movie.title}</h5>
                   <p className="card-text text-muted">{movie.abstract}</p>
                   <div className="mt-auto">
-                    <Link className="btn btn-primary w-100" aria-current="page" to={`/movies/${movie.id}`} > See details </Link>
+                    <Link className="btn btn-outline-primary w-100" to={`/movies/${movie.id}`}> See details</Link>
                   </div>
                 </div>
               </div>
