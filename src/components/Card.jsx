3e605938c32c1b9a05edba434e770a67e8637522
murphy-afk <bardarenoemi@gmail.com>
2026-01-21 16:42:32 +0100

    feat: card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router";
+
+export default function Card({ movie }) {
+  const backendBaseUrl = import.meta.env.VITE_BACKEND_URL;
+  return (
+    <div className="col h-100" key={movie.id}>
+      <div className="card h-100 shadow-sm border-0 movie-card">
+        <img src={movie.image !== null
+          ? `${backendBaseUrl}/movies_cover/${movie.image}`
+          : "no_image_placeholder.jpg"
+        }
+          className="card-img-top"
+          alt={movie.title}
+        />
+        <div className="card-body d-flex flex-column">
+          <span className="badge text-bg-warning w-25 my-1">
+            Vote: {movie.avg_vote}
+          </span>
+          <h5 className="card-title fw-semibold">{movie.title}</h5>
+          <p className="card-text text-muted">{movie.abstract}</p>
+          <div className="mt-auto">
+            <Link className="btn btn-outline-primary w-100" to={`/movies/${movie.id}`}> See details</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
--- src/pages/MovieIndex.jsx
+++ src/pages/MovieIndex.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
+import Card from "../components/Card";
 
 export default function MovieIndex() {
   const [movies, setMovies] = useState([]);
 
+  const backendBaseUrl = import.meta.env.VITE_BACKEND_URL;
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/movies")
+      .get(`${backendBaseUrl}/api/movies`)
       .then((resp) => {
         setMovies(resp.data.results);
       })
@@ -16,7 +18,6 @@ export default function MovieIndex() {
       });
   }, []);
 
-  const backendBaseUrl = import.meta.env.VITE_BACKEND_URL;
 
   return (
     <>
@@ -27,25 +28,7 @@ export default function MovieIndex() {
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
           {movies.map((movie) => (
             <div className="col" key={movie.id}>
-              <div className="card h-100 shadow-sm border-0 movie-card">
-                <img src={movie.image !== null
-                      ? `${backendBaseUrl}/movies_cover/${movie.image}`
-                      : "no_image_placeholder.jpg"
-                  }
-                  className="card-img-top"
-                  alt={movie.title}
-                />
-                <div className="card-body d-flex flex-column">
-                     <span className="badge text-bg-warning w-25 my-1">
-                      Vote: {movie.avg_vote}
-                    </span>
-                  <h5 className="card-title fw-semibold">{movie.title}</h5>
-                  <p className="card-text text-muted">{movie.abstract}</p>
-                  <div className="mt-auto">
-                    <Link className="btn btn-outline-primary w-100" to={`/movies/${movie.id}`}> See details</Link>
-                  </div>
-                </div>
-              </div>
+              <Card movie={movie} />
             </div>
           ))}
         </div>
